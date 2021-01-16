33aba7d7138aee4e7f8c62fbbed36cbcfa6a98a6
Artem Tartmin <ArtemTartmin@gmail.com>
2021-01-16 17:28:00 +0200

    Added Basket Tests

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
@@ -13,21 +16,25 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-           
-        }
+            //Setup
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-        [TestMethod]
-        public void About()
-        {
-            
-        }
 
-        [TestMethod]
-        public void Contact()
-        {
-           
+            //Act
+            productContext.Insert(new Product());
+
+            HomeController controller = new HomeController(productContext,productCategoryContext);
+
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+
+            //Assert
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
+       
     }
 }
